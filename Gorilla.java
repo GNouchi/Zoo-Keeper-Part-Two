d4d049c9469d46d599b38a18883e1e930e400275
GNouchi <gnouchi7@gmail.com>
2018-09-05 21:48:27 -0700

    .

---
+++ Gorilla.java
@@ -0,0 +1,35 @@
+package folderOne.zookeeper;
+
+public class Gorilla extends Mammal{
+	public void eatBananas() {
+		this.setEnergy(this.getEnergy() +10);
+		System.out.println("The gorilla is satisfied, energy level increased (+10)");
+	}
+//temp added for readability
+	public void climb() {
+		int temp = this.getEnergy() -10;
+		if (temp < 0 ) {
+			this.setEnergy(0);
+			System.out.println("The gorilla tried climbing the tree, but ran out of energy");		
+			this.displayEnergy();			
+		}
+		else {
+			this.setEnergy(temp);
+			System.out.println("The gorilla has climbed a tree, energy level decreased (-"+temp+")");		
+		}
+	}
+	public void throwSomething() {
+		int temp = this.getEnergy() -5;
+		if (temp < 0 ) {
+			this.setEnergy(0);
+			System.out.println("The gorilla tried throwing something,but ran out of energy");		
+			this.displayEnergy();
+		}
+		else {
+			this.setEnergy(temp);
+			System.out.println("The gorilla has thrown something, energy level decreased (-"+temp+")");
+		}
+	}
+}
+
+
